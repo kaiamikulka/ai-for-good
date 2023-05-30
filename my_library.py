82ba6df4cfa114b02d0f5bfde8a72fe257928c6d
kaiamikulka <130502396+kaiamikulka@users.noreply.github.com>
2023-05-30 12:32:43 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -51,4 +51,28 @@ def metrics(list):
   accuracy = sum([p==a for p,a in list])/len(list)
   return {'Precision': precision, 'Recall': recall, 'F1': f1, 'Accuracy': accuracy}
 
+def run_random_forest(train, test, target, n):
+  X = up_drop_column(train, target)
+  y = up_get_column(train, target)  
+  k_feature_table = up_drop_column(test, target) 
+  k_actuals = up_get_column(test, target)  
+  clf = RandomForestClassifier(n_estimators=11, max_depth=2, random_state=0)  
+  clf.fit(X, y)  #builds the trees as specified above
+  probs = clf.predict_proba(k_feature_table)
+  pos_probs = [p for n,p in probs]  #probs is list of [neg,pos] like we are used to seeing.
+  pos_probs[:5]
+  all_mets = []
+  for t in thresholds:
+    all_predictions = [1 if pos>t else 0 for pos in pos_probs]
+    pred_act_list = up_zip_lists(all_predictions, k_actuals)
+    mets = metrics(pred_act_list)
+    mets['Threshold'] = t
+    all_mets = all_mets + [mets]
+  all_mets[:2]
+  metrics_table = up_metrics_table(all_mets)
+  metrics_table
+  #your code below
+  print(metrics_table)  #output we really want - to see the table
+  return None
+
 
