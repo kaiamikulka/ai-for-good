648bb7586b5ec5afbe8a001533e1b5a4055b0d40
kaiamikulka <130502396+kaiamikulka@users.noreply.github.com>
2023-06-01 12:19:19 -0700

    Update my_library.py

--- my_library.py
+++ my_library.py
@@ -88,3 +88,23 @@ for arch in all_architectures:
     all_mets[:2]
   print(f'Architecture: {arch}')
   print(up_metrics_table(all_mets))
+  
+  
+  
+def try_archs(full_table, target, architectures, thresholds):
+  train_table, test_table = up_train_test_split(full_table, target, .4)
+  for arch in architectures:
+    all_results = up_neural_net(train_table, test_table, arch, target)
+    #loop through thresholds
+    all_mets = []
+    for t in thresholds:
+      all_predictions = [1 if pos>=t else 0 for neg,pos in all_results]
+      pred_act_list = up_zip_lists(all_predictions, up_get_column(test_table, target))
+      mets = metrics(pred_act_list)
+      mets['Threshold'] = t
+      all_mets += [mets] 
+
+    print(f'Architecture: {arch}')
+    print(up_metrics_table(all_mets))
+  #copy paste code here
+  return None
